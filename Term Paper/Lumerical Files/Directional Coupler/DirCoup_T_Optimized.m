b4df599a9d175decbf5875643d6990f1dabc3c94
Shoumik Chowdhury <shoumikdc@gmail.com>
2022-12-19 13:00:36 -0500

    Backing up Lumerical files from VM

---
+++ Term Paper/Lumerical Files/Directional Coupler/DirCoup_T_Optimized.m
@@ -0,0 +1,7 @@
+load C:/Users/shoumikc/Desktop/6.S976/Term Paper/Directional Coupler/DirCoup_T_Optimized.mat;
+ax1 = axes; 
+plot(lum.x0, lum.y0,lum.x1, lum.y1)
+set(ax1, 'XLim', [1.3e-06 1.7e-06])
+set(ax1, 'YLim', [0 0.8])
+set(ax1,'XGrid', 'on')
+set(ax1,'YGrid', 'on')
